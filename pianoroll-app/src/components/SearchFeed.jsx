5c86b425c29e93128e267f6e3290be06d57b9764
Agata <agata.misiak@procodice.pl>
2023-11-01 11:50:33 +0100

    searching done

--- pianoroll-app/src/components/SearchFeed.jsx
+++ pianoroll-app/src/components/SearchFeed.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const { searchTerm } = useParams();
+
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:
+        <span style={{ color: "#F31503" }}> {searchTerm}</span> rolls.
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
